d2469cf1568e2846757ad7d07a3d9b9b2e71a09f
Caroline Forslund <cf222ev@student.lnu.se>
2017-12-25 17:17:56 +0100

    Working on keyboardButtons

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 //Declare variables
 const qwerty = document.getElementById('qwerty');
 const keyboardButtons = qwerty.getElementsByTagName('button');
-console.log(keyboardButtons[1]);
-// const phraseDiv = document.getElementById('phrase');
 const startButton = document.getElementsByClassName('btn__reset')[0];
 const gameTitle = document.getElementsByClassName('title')[0];
 const phrases = ['Hello World', 'JavaScript is fun', 'Not all those who wander is lost', 'Smile and be happy', 'Work hard Play hard'];
-let missedGuesses = 0;
+// const phraseArray = getRandomPhraseAsArray(phrases);
+
+let missed = 0;
 
 //Attach a event listener to the “Start Game” button to hide the start screen overlay.
 startButton.addEventListener('click', function (event) {
@@ -19,20 +19,24 @@ startButton.addEventListener('click', function (event) {
 
 function getRandomPhraseAsArray(array){
   let phrase = phrases[Math.floor(Math.random()*phrases.length)]; //get random phrase
-  phrase = phrase.split('');  //split sentence into characters
-  addPhraseToDisplay(phrase);
+  const phraseArray = phrase.split('');  //split sentence into characters
+
+  // return characterArray;
+
+  addPhraseToDisplay(phraseArray);
 }
 
 function addPhraseToDisplay(array){
   const phraseDiv = document.getElementById('phrase');
-  const ul = phraseDiv.getElementsByTagName('ul');
-  for (character of phrase){
+  const ul = phraseDiv.getElementsByTagName('ul')[0];
+  for (character of array){
     const li = document.createElement('li');
-    if(!character === ' '){
-      li.addClassName('letter');
+    if(character !== ' '){
+      li.className += 'letter';
     }
+    li.innerHTML = character;
     ul.appendChild(li);
-    li.appendChild(character);
+
   }
 }
 
@@ -40,7 +44,7 @@ function checkLetter(buttonClicked){
   const letters = document.getElementsByClassName('letter').innerHTML();
   for (letter of letters){
     if (letter === buttonClicked){
-      letter.addClassName('show');
+      letter.className += 'show';
       return letter;
     }
     else{
@@ -49,11 +53,22 @@ function checkLetter(buttonClicked){
   }
 }
 
+
+//WHY CAN'T I ADD EVENT LISTENER TO BUTTONS?!?!??!
 for (button of keyboardButtons){
   console.log(button);
   button.addEventListener('click', function(event){
     console.log('click');
-    this.addAttribute('disabled', 'true');
-    checkLetter(button);
+    const letterFound = checkLetter(button);
+    button.addAttribute('disabled', 'true');
+  });
+}
+
+for (let i = 0; i < keyboardButtons.length; i++){
+  const keyboardButton = keyboardButtons[i];
+  keyboardButton.addEventListener('click', function(event){
+    console.log('click');
+    const letterFound = checkLetter(button);
+    keyboardButton.addAttribute('disabled', 'true');
   });
 }
